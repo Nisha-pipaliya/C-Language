8c48797d1f381f7ea74709ab2e805a1e0c02884d
Nisha-Pipaliya <nishapipaliya12@gamil.com>
2023-11-06 20:24:15 +0530

    Ch-1: Introduction to C Language

---
+++ CH-1/MN-1-1.C
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+#include<time.h>
+main()
+{
+	clrscr();
+	printf("Name\t\t:pipaliya nisha\n");
+	sleep(1);
+	printf("Age\t\t:18\n");
+	sleep(1);
+	printf("course\t\t:animation\n");
+	sleep(1);
+	printf("contact\t\t: 123456789\n");
+	sleep(1);
+	getch();
+}
