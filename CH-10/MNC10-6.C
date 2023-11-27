8a7653af6c4869ac74e62babc3680a50b45a5973
Nisha-pipaliya <149695096+Nisha-pipaliya@users.noreply.github.com>
2023-11-27 19:55:10 +0530

    Add files via upload

---
+++ CH-10/MNC10-6.C
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+	int i,j;
+	clrscr();
+
+	for(i=1; i<=5; i++)
+	{
+		for(j=5; j>=i; j--)
+		{
+			if(j%2==1)
+			{
+				printf("1");
+			}
+			else
+			{
+				printf("0");
+			}
+		}
+		printf("\n");
+	}
+
+	getch();
+}
